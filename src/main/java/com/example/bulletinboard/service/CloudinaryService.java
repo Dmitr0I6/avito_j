900a834f0ad2d07634c2f2d280d0dbdd0ef55464
Dmitry <fedyashevdm@gmail.com>
2025-04-22 20:40:21 +0300

    fix CloudinaryService

--- src/main/java/com/example/bulletinboard/service/CloudinaryService.java
+++ src/main/java/com/example/bulletinboard/service/CloudinaryService.java
@@ -5,6 +5,7 @@ import com.cloudinary.Uploader;
 import com.cloudinary.utils.ObjectUtils;
 import io.github.cdimascio.dotenv.Dotenv;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,6 +16,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class CloudinaryService {
     private final Cloudinary cloudinary;
@@ -36,7 +38,6 @@ public class CloudinaryService {
         if (images == null) {
             return Collections.emptyList();
         }
-
         return images.stream()
                 .filter(Objects::nonNull)
                 .map(this::uploadImage)
@@ -47,33 +48,14 @@ public class CloudinaryService {
         if (image == null) {
             throw new IllegalArgumentException("Image file cannot be null");
         }
-
         try {
             Map<?, ?> uploadResult = uploader.upload(image.getBytes(), ObjectUtils.emptyMap());
             return (String) uploadResult.get("url");
         } catch (IOException e) {
+            log.error("Error uploading image to Cloudinary{}", e.getMessage());
             throw new RuntimeException("Failed to upload image to Cloudinary: " + e.getMessage(), e);
         }
     }
-
-//    public CloudinaryService() {
-//        Dotenv dotenv = Dotenv.load();
-//        this.cloudinary = new Cloudinary(dotenv.get("CLOUDINARY_URL"));
-//        System.out.println(cloudinary.config.cloudName);
-//    }
-//
-//    public List<String> uploadImages(List<MultipartFile> images) {
-//        return images.stream().map(this::uploadImage).collect(Collectors.toList());
-//    }
-//
-//    private String uploadImage(MultipartFile image) {
-//        try {
-//            Map<?, ?> uploadResult = cloudinary.uploader().upload(image.getBytes(), ObjectUtils.emptyMap());
-//            return (String) uploadResult.get("url");
-//        } catch (IOException e) {
-//            throw new RuntimeException("Failed to upload image to Cloudinary" + e.getMessage());
-//        }
-//    }
 
 }
 
